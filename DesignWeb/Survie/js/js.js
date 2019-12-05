fadc6678cddd86dcff0e57abe2612dbd8c455f86
Hudoren <sgtjo15@gmail.com>
2019-12-05 13:23:40 -0500

    new

---
+++ DesignWeb/Survie/js/js.js
@@ -0,0 +1,29 @@
+/*js.js */
+/* Programmé par Jonathan Larouche */
+/* Le 21 novembre 2019 */
+
+
+
+$('#Carcajou').click(function () {
+    $('#MdCarc').modal();
+});
+
+
+$('#Renard').click(function () {
+    $('#MdRen').modal();
+});
+
+
+$('#Orignal').click(function () {
+    $('#MdOri').modal();
+});
+
+
+$('#Ours').click(function () {
+    $('#MdOurs').modal();
+});
+
+
+$('#LS').click(function () {
+    $('#MdLS').modal();
+});
