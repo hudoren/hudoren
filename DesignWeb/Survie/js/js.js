7110da95d2c244e50e206619dfc086baba994c15
Hudoren <sgtjo15@gmail.com>
2019-12-16 22:05:16 -0500

    Maj

--- DesignWeb/Survie/js/js.js
+++ DesignWeb/Survie/js/js.js
@@ -3,6 +3,7 @@
 /* Le 21 novembre 2019 */
 
 
+$('#Invalid').hide();
 
 $('#Carcajou').click(function () {
     $('#MdCarc').modal();
@@ -27,3 +28,26 @@ $('#Ours').click(function () {
 $('#LS').click(function () {
     $('#MdLS').modal();
 });
+
+
+$("#Email").keyup(function() {
+
+    var email = $("#Email").val();
+
+
+    if (Comm(email) && Point(email)){
+        $("#Invalid").hide();
+    }
+    else {
+        $('#Invalid').show();
+    }
+
+});
+
+function Comm(str) {
+    return (/[@]/.test(str));
+}
+
+function Point(str) {
+    return (/[.]/.test(str));
+}
